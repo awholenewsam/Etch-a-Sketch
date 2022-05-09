e5fb4610a1b1cdc63b23b692066d6db2b63bd809
awholenewsam <102858442+awholenewsam@users.noreply.github.com>
2022-05-09 09:57:41 +0200

    Add ui

--- javascript.js
+++ javascript.js
@@ -1,19 +1,66 @@
-const cdiv = document.querySelector('.grid');
+const grid = document.getElementById('boxes');
 
-// Add 16 divs
-for (let i = 1; i < 257; i++) {
-  const div = document.createElement('div');
-  div.style.cssText = "border: 1px solid black; height: 25px; width: 25px";  
-  cdiv.appendChild(div);
+function makeGrid(rows, cols) {
+    grid.style.setProperty('--grid-rows', rows);
+    grid.style.setProperty('--grid-cols', cols);
+    grid.style.width = "640px";
+    grid.style.height = "640px"
+    grid.style.overflow = "hidden";
+    for (i = 0; i < rows * cols; i++) {
+        let cell = document.createElement("div");
+        grid.appendChild(cell).className = "grid-item"; 
+        cell.addEventListener("mouseover", () => {
+            cell.style.backgroundColor = "black";
+        });
+    };
+};
+
+function removeAllChildNodes(parent) {
+    while(parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
 }
 
-cdiv.addEventListener('mouseover', e => e.target.classList.add('my-color-class'))
+const slider = document.querySelector('#slider')
+const screenVal = document.querySelector('.value');
+slider.addEventListener('input', function(){
+    let val = document.getElementById('slider').value;
+    screenVal.textContent = val;
+    removeAllChildNodes(grid);
+    grid.setAttribute('style', `grid-template-columns: repeat(${val}, 2fr); grid-template-rows: repeat(${val}, 2fr);`);
+    for (let i = 0; i < val*val; i++) {
+        const div = document.createElement('div');
+        div.classList.add('cell');
+        div.addEventListener('mouseover', function(event){
+            event.target.style.backgroundColor = 'black';
+        })
+        grid.appendChild(div); 
+    }
+}); 
 
+function randomColor()
+{
+     color ='rgb('+Math.round(Math.random()*255)+','+Math.round(Math.random()*255)+','+Math.round(Math.random()*255)+')';
 
+     return color;
+}
 
+const rainbow = document.querySelector('#randomColor');
+rainbow.addEventListener('click', function() {
+    let val = document.getElementById('slider').value;
+    let cell = grid.children;
+    for (let i = 0; i < val*val; i++) {
+        cell[i].addEventListener('mouseover', function(event){
+            event.target.style.backgroundColor = randomColor();
+        })
+    }
+});
 
 
 const reset = document.querySelector('.reset');
 reset.addEventListener('click', function(){
      window.location.reload();
 });
+
+
+makeGrid(16,16);
